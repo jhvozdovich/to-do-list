3377f68482f724e5c6339c2a1e8ab6986d1da17d
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-11 16:07:56 -0700

    add test for getting item description

--- ToDoList.Solution/ToDoList/Models/Item.cs
+++ ToDoList.Solution/ToDoList/Models/Item.cs
@@ -2,10 +2,14 @@ namespace ToDoList.Models
 {
   public class Item
   {
-
+    public string Description { get; }
+    public Item(string description)
+    {
+      Description = description;
+    }
   public static void Main()
-  {
-    
-  }
+    {
+      
+    }
   }
 }
