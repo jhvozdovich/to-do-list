af8735dfcb5b125232a184c3e13e7bc05ae29f9b
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-11 16:11:52 -0700

    add test to change description of item

--- ToDoList.Solution/ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Solution/ToDoList.Tests/ModelTests/ItemTests.cs
@@ -24,5 +24,19 @@ namespace ToDoList.Tests
       // Assert
       Assert.AreEqual(description, result);
     }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      // Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      // Act
+      string updatedDescription = "Do the dishes.";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+      // Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
   }
 }
